db1acbb94b7c061b409d4f24c4f5b36724cad4bc
soyekwon <soye0710@naver.com>
2022-04-30 02:32:37 +0900

    feat: get, post, put, delete

---
+++ client.py
@@ -0,0 +1,55 @@
+import socket
+
+
+# 서버의 주소
+HOST = '127.0.0.1'  
+# 서버에서 지정해 놓은 포트 번호
+PORT = 9999       
+
+
+# 소켓 객체 생성
+# 주소 체계(address family) -> IPv4, SOCK_STREAM -> TCP 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# 지정한 HOST와 PORT를 사용하여 서버와 연결 
+client_socket.connect((HOST, PORT))
+
+while True:
+	print("===================================  SoyE Todo List  ===================================")
+	print("1. todo list 불러오기 ", "2. todo 추가 ", "3. todo 수정 ", "4. todo 삭제", "5. header 정보 요청")
+	num = int(input("input number: "))
+	todo = ""
+
+	if num == 1:
+		request_data = 'GET / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
+	elif num == 2:
+		todo = input("Add todo:")
+		request_data = 'POST / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
+	elif num == 3:
+		# out of index 일때 에러 추가
+		todo_num = input("Input update todo number: ")
+		todo = input("Input update todo: ")
+		todo += " "
+		todo += todo_num
+		request_data = 'PUT / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
+	elif num == 4:
+		todo = input("Input delete todo number:")
+		request_data = 'DELETE / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
+	elif num == 5:
+		request_data = 'HEAD / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
+	else:
+		break
+
+	request_data = request_data.format(len(todo), todo)
+	# 데이터 전송
+	client_socket.send(request_data.encode('utf-8'))
+	# 데이터 수신
+	data = client_socket.recv(1024)
+	print("")
+	print("response code:", end=" ")
+	print(data.decode())	
+
+
+# 소켓을 닫습니다.
+client_socket.close()
---
+++ server.py
@@ -0,0 +1,79 @@
+import socket
+
+
+# localhost
+HOST = '127.0.0.1'
+# 포트 번호
+PORT = 9999        
+
+
+# 소켓 객체 생성
+# 주소 체계(address family)로 IPv4, SOCK_STREAM -> TCP 사용
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# 포트 사용중이라 연결할 수 없다는 
+# WinError 10048 에러 해결를 위해 필요 
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+# host와 port 맵핑  
+server_socket.bind((HOST, PORT))
+
+# 맵핑된 소켓을 연결 요청 대기 상태로 전환
+server_socket.listen()
+
+print("대기중입니다... ")
+
+# accept 함수에서 대기하다가 클라이언트가 접속하면 새로운 소켓을 리턴 
+client_socket, addr = server_socket.accept()
+
+print(addr, " 에서 연결되었습니다.")
+
+todoList = ["컴네 플젝", "DB report"]
+
+# 무한루프를 돌면서 
+while True:
+
+    # 클라이언트가 보낸 데이터 수신, 최대 1024
+    data = client_socket.recv(1024)
+
+    # 빈 문자열을 수신하면 루프 중지
+    if not data:
+        break
+
+    request_data = data.decode().split()
+    print(request_data)
+    method = request_data[0]
+    
+    todos = "\n"
+
+    for i in range(len(todoList)):
+        todos += todoList[i]
+        todos += "\n"
+
+    if method == "GET":
+        response_data = "{} 200 OK\n\nTodoList: {}".format(method, todos)
+    elif method == "POST":
+        new_todo = request_data[19].split(':')[1]  # 추가할 todo 내용
+        todoList.append(new_todo)  # todo 추가
+        response_data = "{} 200 OK\n".format(method)
+    elif method == "PUT":
+        update_todo = request_data[19].split(':')[1]  #  수정할 todo 내용
+        idx = int(request_data[20])  # 수정할 index 
+        todoList[idx-1] = update_todo  # todo list 수정
+        response_data = "{} 200 OK\n".format(method)
+    elif method == "DELETE":
+        idx = int(request_data[19].split(':')[1])  #  수정할 todo 내용
+        del todoList[idx-1]
+        response_data = "{} 200 OK\n".format(method)
+
+    # 받은 문자열을 다시 클라이언트로 전송(에코) 
+    print(response_data)
+    client_socket.send(response_data.encode())
+    print("데이터를 클라이언트에게 보냈습니다.")
+
+
+# 소켓을 닫습니다.
+client_socket.close()
+server_socket.close()
