11423f6c396ab3c53f658deb183ab725c28dd3e2
soyekwon <soye0710@naver.com>
2022-05-01 02:15:51 +0900

    feat: HEAD method, add response message header

--- client.py
+++ client.py
@@ -1,54 +1,62 @@
 import socket
+from urllib import response
 
 
-# 서버의 주소
-HOST = '127.0.0.1'  
-# 서버에서 지정해 놓은 포트 번호
-PORT = 9999       
+HOST = '127.0.0.1'  # 서버의 주소
+PORT = 9999  # 서버에서 지정해 놓은 포트 번호
 
-
-# 소켓 객체 생성
-# 주소 체계(address family) -> IPv4, SOCK_STREAM -> TCP 
+# 소켓 객체 생성(주소 체계 -> IPv4, SOCK_STREAM -> TCP)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-# 지정한 HOST와 PORT를 사용하여 서버와 연결 
-client_socket.connect((HOST, PORT))
+client_socket.connect((HOST, PORT))  # 지정한 HOST와 PORT를 사용하여 서버와 연결
 
 while True:
-	print("==========================================  SoyE Todo List  ==========================================")
-	print("1. list 불러오기(get) ", "2. todo 추가(post) ", "3. todo 수정(put) ", "4. todo 삭제(delete)", "5. header 요청(head)", "6. 종료")
-	num = int(input("input number: "))
-	todo = ""
+    print("")
+    print("==============================================  SoyE Todo List  ==============================================")
+    print("1. list 불러오기(get) ", "2. todo 추가(post) ", "3. todo 수정(put) ", "4. todo 삭제(delete)", "5. header 요청(head)", "6. 종료")
+    num = int(input("input number: "))
+    todo = ""
 
-	if num == 1:
-		request_data = 'GET / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
-	elif num == 2:
-		todo = input("Add todo: ")
-		request_data = 'POST / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
-	elif num == 3:
-		# out of index 일때 에러 추가
-		todo_num = input("Input update todo number: ")
-		todo = input("Input update todo: ")
-		todo += " "
-		todo += todo_num
-		request_data = 'PUT / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
-	elif num == 4:
-		todo = input("Input delete todo number:")
-		request_data = 'DELETE / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
-	elif num == 5:
-		request_data = 'HEAD / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nContent-Length:{}\r\nmessage:{}'
-	elif num == 6:
-		break
+    if num == 1:
+        request_data = 'GET / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nConnection: keep-alive\r\nContent-Length:{}\r\nmessage:{}'
+    elif num == 2:
+        todo = input("Add todo: ")
+        request_data = 'POST / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nConnection: keep-alive\r\nContent-Length:{}\r\nmessage:{}'
+    elif num == 3:
+        todo_num = input("Input update todo number: ")
+        todo = input("Input update todo: ")
+        todo += " "
+        todo += todo_num
+        request_data = 'PUT / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nConnection: keep-alive\r\nContent-Length:{}\r\nmessage:{}'
+    elif num == 4:
+        todo = input("Input delete todo number:")
+        request_data = 'DELETE / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nConnection: keep-alive\r\nContent-Length:{}\r\nmessage:{}'
+    elif num == 5:
+        request_data = 'HEAD / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36\r\nConnection: keep-alive\r\nContent-Length:{}\r\nmessage:{}'
+    elif num == 6:
+        break
 
-	request_data = request_data.format(len(todo), todo)
-	# 데이터 전송
-	client_socket.send(request_data.encode('utf-8'))
-	# 데이터 수신
-	data = client_socket.recv(1024)
-	print("")
-	print("response message:", end=" ")
-	print(data.decode())	
+    request_data = request_data.format(len(todo), todo)
+    client_socket.send(request_data.encode('utf-8'))  # 데이터 전송
+    data = client_socket.recv(1024)  # 데이터 수신
+
+    print("")
+
+    if num == 1:
+        response_data = data.decode().split("\r\n\r\n")[1].split("\r\n")  # response message에서 body부분 추출
+        status_code = response_data[0].split(':')[1].lstrip()  # body에서 status code 추출
+        print("status code: ", end="")
+        print(status_code)
+        todo_list = response_data[1].split(':')[1].lstrip()   # body에서 todolist 추출
+        print("")
+        print("todo list: ")
+        print(todo_list)
+    elif num == 5:
+        response_data = data.decode().split("\r\n\r\n")[0]
+        print(response_data)
+    else:
+        status_code = data.decode().split("\r\n\r\n")[1].split(":")[1]  # body에서 status code 추출
+        print("status code: ", end="")
+        print(status_code)
 
 
 # 소켓을 닫습니다.
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+import time
 
 # localhost
 HOST = '127.0.0.1'
@@ -43,6 +43,7 @@ while True:
 
     f = open("todolist.txt", 'r', encoding="UTF-8")  # todolist.txt 열고
     todoList = []
+
     while True:
         line = f.readline()  # 파일 읽어서
         if not line:
@@ -52,7 +53,6 @@ while True:
     f.close()
 
     request_data = data.decode().split()
-    print(request_data)
     method = request_data[0]
 
     todos = "\n"
@@ -61,11 +61,11 @@ while True:
         todos += todoList[i]
 
     if method == "GET":
-        response_data = "{} 200 OK\n\nTodoList: {}".format(method, todos)
+        response_data = "HTTP/1.1 200 OK\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}\r\ntodo: {}".format(time.ctime(time.time()), "200 OK", todos)
     elif method == "POST":
-        new_todo = request_data[19].split(':')[1]  # 추가할 todo 내용
+        new_todo = request_data[-1].split(':')[1]  # 추가할 todo 내용
         if new_todo == "":  # no_content 이면
-            response_data = "{} 204 No Content\n".format(method)
+            response_data = "HTTP/1.1 204 No Content\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "204 No Content")
         else:
             f = open("todolist.txt", 'w', encoding="UTF-8")
             for i in range(len(todoList)):
@@ -73,14 +73,13 @@ while True:
             f.write("\n")
             f.write(new_todo)
             f.close()
-            response_data = "{} 201 Created\n".format(method)
+            response_data = "HTTP/1.1 201 Created\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "201 Created")
     elif method == "PUT":
-        print(todoList[0])
-        update_todo = request_data[19].split(':')[1]  # 수정할 todo 내용
-        idx = int(request_data[20])  # 수정할 index
+        update_todo = request_data[-2].split(':')[1]  # 수정할 todo 내용
+        idx = int(request_data[-1])  # 수정할 index
         if todoList[idx-1] == update_todo:  # 입력한 todo와 원래 todo와 같은 내용이면
-            response_data = "{} 304 Not Modified because of same content\n".format(
-                method)
+            response_data = "HTTP/1.1 304 Not Modified \r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "304 Not Modified")
+                
         else:
             f = open("todolist.txt", 'w', encoding="UTF-8")
             for i in range(idx-1):
@@ -90,21 +89,22 @@ while True:
             for i in range(idx, len(todoList)):
                 f.write(todoList[i])
             f.close()
-            response_data = "{} 202 Accepted\n".format(method)
+            response_data = "HTTP/1.1 202 Accepted\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "202 Accepted")
     elif method == "DELETE":
-        idx = int(request_data[19].split(':')[1])  # 수정할 todo 내용
+        idx = int(request_data[-1].split(':')[1])  # 수정할 todo 내용
         if idx > len(todoList):  # index의 범위를 벗어나면
-            response_data = "{} 400 Bad Request because of out of index\n".format(
-                method)
+            response_data = "HTTP/1.1 400 Bad Request\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "400 Bad Request")
         else:
             del todoList[idx-1]
             f = open("todolist.txt", 'w', encoding="UTF-8")
             for i in range(len(todoList)):
                 f.write(todoList[i])
             f.close()
-            response_data = "{} 200 OK\n".format(method)
+            response_data = "HTTP/1.1 200 OK\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "200 OK")
+    elif method == "HEAD":
+        response_data = "HTTP/1.1 200 OK\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "200 OK")
     else:  # other method 입력하면
-        response_data = "{} 405 Method Not Allowed\n".format(method)
+        response_data = "HTTP/1.1 405 Method Not Allowed\r\nDate: {}\r\nServer: python3(windows10)\r\nConnection: Keep-Alive\r\n\r\nstatus_code: {}".format(time.ctime(time.time()), "405 Method Not Allowed")
     # 받은 문자열을 다시 클라이언트로 전송(에코)
     print(response_data)
     client_socket.send(response_data.encode())
